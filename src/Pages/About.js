eeaeafae0af2aedd3e0807d1bd6b2aac234818e5
jac-lene <jaclenesini@gmail.com>
2022-08-06 22:19:53 -0400

    tweaks commit before close

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -5,11 +5,19 @@ import { Image } from 'react-bootstrap'
 function About() {
   return (
     <div className='center'>
+        <div className='spacer'></div>
+        <div className='spacer'></div>
         <div className='about-cont'>
             <Image src={Me} rounded style={{width:'600px'}} className='about-img'/>
             <br/>
             <div className='about-txt'>
-                <h2>I'm Jaclene.</h2>
+                <h2>Hey there! Jaclene here.</h2> 
+                <h4>My pronouns are they/them.</h4>
+                <br/>
+                <h4>I'm a people focused programmer with a desire to produce compassionate, engaging, and empowering work that builds community and brings joy. 
+                <br/> <br/> 
+                Let's work together and create something amazing.
+                </h4>
             </div>
             
         
