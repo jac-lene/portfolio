96d275277c227e24a32374de6f0b4e699d04bf7b
jac-lene <jaclenesini@gmail.com>
2022-08-09 21:40:50 -0400

    played with styling more

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,12 +1,30 @@
 import React from 'react'
-import Me from '../Images/srs.jpg'
 import { Image } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Me from '../Images/srs.jpg'
+import basics from '../Images/icons/3.png'
+import boot from '../Images/icons/boot.png'
+import django from '../Images/icons/django.png'
+import exp from '../Images/icons/express.png'
+import mongo from '../Images/icons/mongo.png'
+import node from '../Images/icons/node.jpeg'
+import postgres from '../Images/icons/postgres.png'
+import python from '../Images/icons/python.png'
+import pythonb from '../Images/icons/pythonbig.png'
+import react from '../Images/icons/react.png'
+import reactb from '../Images/icons/reactbig.png'
+import vscode from '../Images/icons/vscode.png'
 
 function About() {
   return (
     <div className='center'>
         <div className='spacer'></div>
         <div className='spacer'></div>
+
+        <div className='about-cont1'>
         <div className='about-cont'>
             <Image src={Me} rounded style={{width:'600px'}} className='about-img'/>
             <br/>
@@ -16,14 +34,51 @@ function About() {
                 <br/>
                 <h4>I'm a people focused programmer with a desire to produce compassionate, engaging, and empowering work that builds community and brings joy. 
                 <br/> <br/> 
-                Let's work together and create something amazing.
+                Let's work together to create something amazing.
                 </h4>
             </div>
-            
-        
-        
+        </div>
 
+        <div className='spacer'></div>
+        <div className='spacer'></div>
 
+        <div>
+            <h2>Programs I work with:</h2>
+        </div>
+
+        <div className='spacer'></div>
+
+        <Container className='about-grid'>
+      <Row>
+        <Col>
+          <Image src={basics} rounded style={{width:'300px'}}/>
+        </Col>
+        <Col>          
+          <Image src={reactb} rounded style={{width:'100px'}}/>
+        </Col>
+        <Col>          
+          <Image src={django} rounded style={{width:'100px'}}/>
+        </Col>
+        <Col>          
+          <Image src={postgres} rounded style={{width:'100px'}}/>
+        </Col>
+        <Col>          
+          <Image src={pythonb} rounded style={{width:'100px'}}/>
+        </Col>
+        <Col>          
+          <Image src={boot} rounded style={{width:'100px'}}/>
+        </Col>
+        <Col>          
+          <Image src={mongo} rounded style={{width:'100px'}}/>
+        </Col>
+        <Col>          
+          <Image src={vscode} rounded style={{width:'100px'}}/>
+        </Col>
+      </Row>
+    </Container>
+
+    <div className='spacer'></div>
+    <div className='spacer'></div>
 
         </div>
     
